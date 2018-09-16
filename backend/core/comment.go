a92af99374c59948a9cd913d3a58a3921cb23173
thanhpham <phamthethanh1088@gmail.com>
2018-09-16 22:48:06 +0700

    temp comment and vote servicers

---
+++ backend/core/comment.go
@@ -0,0 +1,24 @@
+package core
+
+import (
+	"context"
+	"time"
+)
+
+// Comment represent content of a comment
+type Comment struct {
+	ID          string    `json:"id,omitempty"`
+	ArticleID   string    `json:"article_id,omitempty"`
+	Content     string    `json:"content,omitempty"`
+	CreatedBy   string    `json:"created_by,omitempty"`
+	CreatedByID string    `json:"created_by_id,omitempty"`
+	LastUpdate  time.Time `json:"last_update,omitempty"`
+}
+
+// CommentServicer provide operations on comments
+type CommentServicer interface {
+	ListByArticle(ctx context.Context, articleID string) ([]*Comment, error)
+	Update(ctx context.Context, c *Comment) error
+	Delete(ctx context.Context, id string) error
+	Create(ctx context.Context, c *Comment) (id string, err error)
+}
---
+++ backend/core/vote.go
@@ -0,0 +1,16 @@
+package core
+
+import "context"
+
+// Vote represent content of a vote for a specific article
+type Vote struct {
+	ID          string `json:"id,omitempty"`
+	ArticleID   string `json:"article_id,omitempty"`
+	CreatedBy   string `json:"created_by,omitempty"`
+	CreatedByID string `json:"created_by_id,omitempty"`
+}
+
+//VoteServicer provide operations on voting
+type VoteServicer interface {
+	Create(ctx context.Context, articleID string) (id string, err error)
+}
